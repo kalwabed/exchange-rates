c379c6268982a55ab9bd44fb27dfc1eb96a07c49
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-24 21:21:48 +0700

    add simple unit testing

---
+++ src/test-utils.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import store from './store/main'
+
+const ReduxProvider = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
+const reduxRender = (ui, options) => render(ui, { wrapper: ReduxProvider, ...options })
+
+export * from '@testing-library/react'
+
+export { reduxRender as render }
